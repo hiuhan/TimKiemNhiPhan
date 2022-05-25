c2bceeecdfe3313ac781b93d7017ac4e08aef762
hiuhan <hieulc638@gmail.com>
2022-05-25 14:08:50 +0700

    up

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import static java.util.Arrays.binarySearch;
+
+public class Main {
+    private static long[] list;
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));
+        System.out.println(binarySearch(list, 11));
+        System.out.println(binarySearch(list, 79));
+        System.out.println(binarySearch(list, 1));
+        System.out.println(binarySearch(list, 5));
+        System.out.println(binarySearch(list, 80));
+    }
+}
